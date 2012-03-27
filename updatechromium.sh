e657cb57fcd37519cd3afa68d32a0f546b3167ae
kv0th3 <dean.kariniemi@gmail.com>
2012-03-27 11:43:54 -0500

    updated script to always save file as chrome-win32.zip

--- updatechromium.sh
+++ updatechromium.sh
@@ -17,18 +17,18 @@ fi
 
 echo "http://commondatastorage.googleapis.com/chromium-browser-snapshots/Win/$LATEST/chrome-win32.zip"
 echo -n "Downloading build for $LATEST... "
-wget http://commondatastorage.googleapis.com/chromium-browser-snapshots/Win/$LATEST/chrome-win32.zip
+wget -O chrome-win32.zip http://commondatastorage.googleapis.com/chromium-browser-snapshots/Win/$LATEST/chrome-win32.zip
 
 if [ $? -ne 0 ]
 then 
 	echo "chrome-win32.zip not found, aborting.."
 else 
 	echo "done!"
-		
+	
 	echo -n "Deleting old install... "
 	rm -rf chrome-win32
 	echo "done!"
-
+	
 	echo -n "Unpacking new install... "
 	unzip -q chrome-win32.zip
 	echo "done!"
