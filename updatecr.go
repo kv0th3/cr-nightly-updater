20b22188f53cb4137bda72bc64875eb525368656
kv0th3 <dean.kariniemi@gmail.com>
2012-10-15 12:25:32 +0300

    completely fresh install each time, remove *.zip after extracting

--- updatecr.go
+++ updatecr.go
@@ -18,28 +18,31 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Latest Chromium snapshot build number is %s\n", last_change)
+	//fmt.Printf("Latest Chromium snapshot build number is %s\n", last_change)
 	url := "https://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux/" + string(last_change) + "/chrome-linux.zip"
 
-	fmt.Printf("url: %s\n", url)
+	//fmt.Printf("download url: %s\n", url)
 
 	res_download, err := http.Get(url)
-
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	data, err := ioutil.ReadAll(res_download.Body)
-
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	res_download.Body.Close()
 
 	ioutil.WriteFile("chrome-linux.zip", data, 0666)
 
-	fmt.Printf("Downloaded the latest Chromium build %s\n", last_change)
+	fmt.Printf("Downloaded chrome-linux.zip build:%s\n", last_change)
+
+	cmdremove := exec.Command("rm", "-Rf", "./chrome-linux/")
+	errremove := cmdremove.Run()
+	if errremove != nil {
+		log.Fatal(errremove)
+	}
+	println("Removed chrome-linux/")
 
 	cmd := exec.Command("unzip", "-o", "chrome-linux.zip")
 	err3 := cmd.Run()
@@ -47,4 +50,11 @@ func main() {
 		log.Fatal(err3)
 	}
 	println("Unzipped chrome-linux.zip")
+
+	cmd2 := exec.Command("rm", "chrome-linux.zip")
+	err4 := cmd2.Run()
+	if err4 != nil {
+		log.Fatal(err4)
+	}
+	println("Deleted chrome-linux.zip")
 }
